c79844c0a5390e4e1e9f43127b31c537387eb236
Sachin Kumar <56272723+hackcoderr@users.noreply.github.com>
2020-09-05 12:14:25 +0530

    Add files via upload

---
+++ lib/premium.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Premium extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Premium'),
+    );
+  }
+}
